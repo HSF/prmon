bdf2cc98471f4570873f369a08a4f9b90963f5df
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2018-08-16 13:50:38 +0200

    Add Travis CI setup file
    Make Travis CI test two different environments, via
    the "env" stanza to setup for a CentOS7 and an Ubuntu 16
    containerised image.
    
    Generalise the build script to allow for easy customisation of the
    build within different images.
    
    Add a small README to the tests area.

---
+++ .travis-scripts/build-test.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+#
+# Wrapper script to build prmon on travis and run all tests
+# 
+# Note that CXX, CC and CMAKE can all be customised so that
+# they can be matched to the development environment in each
+# container
+cd /tmp
+if [ -z "$CXX" ]; then
+    CXX=$(type -p g++)
+fi
+if [ -z "$CC" ]; then
+	CC=$(type -p gcc)
+fi
+if [ -z "$CMAKE" ]; then
+	CMAKE=$(type -p cmake)
+fi
+$CMAKE -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_C_COMPILER=$CC /mnt
+make -j
+make test
