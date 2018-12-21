57edb228c171650b055ab7fce0f67469e476446f
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2018-12-21 13:52:37 +0100

    More CI script tweaks

--- .travis-scripts/build-test.sh
+++ .travis-scripts/build-test.sh
@@ -4,7 +4,8 @@
 #
 # Note that CXX, CC and CMAKE can all be customised so that
 # they can be matched to the development environment in each
-# container
+# container; CMAKE_EXTRA can be used to pass special options
+# to cmake for particular platforms
 cd /tmp
 if [ -z "$CXX" ]; then
     CXX=$(type -p g++)
@@ -15,7 +16,10 @@ fi
 if [ -z "$CMAKE" ]; then
 	CMAKE=$(type -p cmake)
 fi
-echo $CMAKE -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_C_COMPILER=$CC $CMAKE_EXTRA /mnt
-$CMAKE -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_C_COMPILER=$CC $CMAKE_EXTRA /mnt
-make -j
+
+# For debugging, handy to print what we're doing
+cmd="$CMAKE -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_C_COMPILER=$CC $CMAKE_EXTRA /mnt"
+echo $cmd
+$cmd
+make -j 4
 make test
