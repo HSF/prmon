3ee83e606bf5ad3bf17d37d8dc7193414bba0b58
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2018-12-21 11:09:01 +0100

    Fixes for nlohmann_json on Ubuntu18
    Adapt CI scripts to run properly on Ubuntu18 (need extra CMake arugment)
    Small README improvements

--- .travis-scripts/build-test.sh
+++ .travis-scripts/build-test.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 #
 # Wrapper script to build prmon on travis and run all tests
-# 
+#
 # Note that CXX, CC and CMAKE can all be customised so that
 # they can be matched to the development environment in each
 # container
@@ -15,6 +15,6 @@ fi
 if [ -z "$CMAKE" ]; then
 	CMAKE=$(type -p cmake)
 fi
-$CMAKE -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_C_COMPILER=$CC /mnt
+$CMAKE -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_C_COMPILER=$CC $CMAKE_EXTRA /mnt
 make -j
 make test
