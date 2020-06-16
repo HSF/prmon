9ea38972f2e4bc2fd6c87d3ffc3cc1f60f4162d8
Alaettin Serhan Mete <serhanmete@gmail.com>
2020-06-16 21:22:24 +0200

    Drop the custom action and use u20-dev container to execute make clang-format

---
+++ .github/scripts/clang-format.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+#
+# Wrapper script to run make clang-format 
+# The assumption is that we're running in
+# hepsoftwarefoundation/u20-dev
+
+# Setup
+echo ">> Setting things up..."
+CXX=`which clang++-10`
+CC=`which clang-10`
+CMAKE=`which cmake`
+
+# Configure cmake from /tmp
+echo ">> Running the cmake configuration..."
+cd /tmp
+cmd="$CMAKE -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_C_COMPILER=$CC /mnt"
+echo $cmd
+$cmd
+
+# Run clang-format target
+echo ">> Running make clang-format..."
+make clang-format
