06c7b747e7bb33d6aed7c7f2396571cdca49b186
clang-format <clang-format@github-actions>
2021-07-28 07:52:28 +0000

    Apply clang-format

--- package/src/parameter.cpp
+++ package/src/parameter.cpp
@@ -1,17 +1,18 @@
 // Copyright (C) 2021 CERN
 // License Apache2 - see LICENCE file
 
-#include <iostream>
-
 #include "parameter.h"
 
+#include <iostream>
+
 namespace prmon {
-  int monitored_value::set_value(mon_value new_value) {
-    if (monotonic && (new_value < value)) {
-      std::cerr << "Error: attempt to reduce the monitored value of " << name << " (which is monotonic)" << std::endl;
-      return 1;
-    }
-    value = new_value;
-    return 0;
+int monitored_value::set_value(mon_value new_value) {
+  if (monotonic && (new_value < value)) {
+    std::cerr << "Error: attempt to reduce the monitored value of " << name
+              << " (which is monotonic)" << std::endl;
+    return 1;
   }
+  value = new_value;
+  return 0;
 }
+}  // namespace prmon
