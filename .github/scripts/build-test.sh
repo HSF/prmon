e633e8d3bb43501b7c31c567e02a4cf509d0ac28
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2021-12-14 13:54:27 +0100

    Add tests with clang
    For the c8-dev platform activate also a build with clang

--- .github/scripts/build-test.sh
+++ .github/scripts/build-test.sh
@@ -1,16 +1,24 @@
 #! /usr/bin/env bash
 #
-# Wrapper script to build prmon on travis and run all tests
+# Wrapper script to build prmon and run all tests
 #
 # Note that CXX, CC and CMAKE can all be customised so that
 # they can be matched to the development environment in each
 # container; CMAKE_EXTRA can be used to pass special options
 # to cmake for particular platforms
 cd /tmp
+echo "Starting container run for platform $PLATFORM, compiler suite $COMPILER"
 if [ -z "$CXX" ]; then
+	if [ $COMPILER == "clang" ]; then
+		CXX=$(type -p clang++)
+	fi
+	# gcc suite is the fall through
     CXX=$(type -p g++)
 fi
 if [ -z "$CC" ]; then
+	if [ $COMPILER == "clang" ]; then
+		CXX=$(type -p clang)
+	fi
 	CC=$(type -p gcc)
 fi
 if [ -z "$CMAKE" ]; then
