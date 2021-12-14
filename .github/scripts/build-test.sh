0e8c9a30ab03ec076e4d3544cafb24f4d59c6a50
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2021-12-14 16:21:24 +0100

    Clang CI option (#213)
    * Add tests with clang
    
    For the c8-dev platform activate also a build with clang
    
    * Typo on docker command
    
    * Set workflow environment properly for CI scripts
    
    * Fix typo when setting CC/CXX for clang suite
    
    Improve echo texts
    
    * Don't reset compilers by mistake!
    
    * More consistent passing of variables
    
    Add output in those cases where CXX and CC are set from the outside
    
    * Change to space indented

--- .github/scripts/build-test.sh
+++ .github/scripts/build-test.sh
@@ -1,20 +1,37 @@
 #! /usr/bin/env bash
 #
-# Wrapper script to build prmon on travis and run all tests
+# Wrapper script to build prmon and run all tests
 #
 # Note that CXX, CC and CMAKE can all be customised so that
 # they can be matched to the development environment in each
 # container; CMAKE_EXTRA can be used to pass special options
 # to cmake for particular platforms
 cd /tmp
+echo "Starting build and test for platform $PLATFORM, compiler suite $COMPILER"
 if [ -z "$CXX" ]; then
-    CXX=$(type -p g++)
+    if [ "$COMPILER" == "clang" ]; then
+        CXX=$(type -p clang++)
+    else
+        # gcc suite is the fall through
+        CXX=$(type -p g++)
+    fi
+else
+    echo "CXX was set externally to $CXX"
 fi
+
 if [ -z "$CC" ]; then
-	CC=$(type -p gcc)
+    if [ "$COMPILER" == "clang" ]; then
+        CC=$(type -p clang)
+    else
+        # gcc suite is the fall through
+        CC=$(type -p gcc)
+    fi
+else
+    echo "CC was set externally to $CC"
 fi
+
 if [ -z "$CMAKE" ]; then
-	CMAKE=$(type -p cmake)
+    CMAKE=$(type -p cmake)
 fi
 
 # For debugging, handy to print what we're doing
