ccdd02bfe137be9ba0401dc7e1efcd02cb0da332
Graeme Stewart <graemes.cern@gmail.com>
2024-05-03 12:46:09 +0200

    Parse ccpm field as string
    This can be a "-" instead of 0

--- package/src/nvidiamon.cpp
+++ package/src/nvidiamon.cpp
@@ -74,9 +74,9 @@ void nvidiamon::update_stats(const std::vector<pid_t>& pids,
   }
 
   // Loop over output
-  unsigned int gpu_idx{}, sm{}, mem{}, fb_mem{}, ccpm{};
+  unsigned int gpu_idx{}, sm{}, mem{}, fb_mem{};
   pid_t pid{};
-  std::string enc{}, dec{}, jpg{}, ofa{}, cg_type{}, cmd_name{};
+  std::string enc{}, dec{}, jpg{}, ofa{}, cg_type{}, ccpm{}, cmd_name{};
   std::unordered_map<unsigned int, bool>
       activegpus{};  // Avoid double counting active GPUs
   for (const auto& s : cmd_result.second) {
