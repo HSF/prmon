22df64f08b3fbadd8342f4d8696924a8735d1d07
EyupKeremBas7 <baseyupkerem@gmail.com>
2026-02-11 21:52:30 +0300

    backup

--- package/src/nvidiamon.h
+++ package/src/nvidiamon.h
@@ -3,19 +3,21 @@
 // NVIDIA GPU monitoring class
 //
 
-#ifndef PRMON_NVIDIAMON_H
+#ifndef PRMON_NVIDIAMON_H 
 #define PRMON_NVIDIAMON_H 1
 
 #include <map>
 #include <string>
 #include <vector>
+#include <nvml.h>
 
 #include "Imonitor.h"
 #include "MessageBase.h"
 #include "parameter.h"
 #include "registry.h"
 
-class nvidiamon final : public Imonitor, public MessageBase {
+
+class nvidiamon final : public Imonitor, public MessageBase { 
  private:
   // const static std::vector<std::string> default_nvidia_params{
   //   "ngpus", "gpusmpct", "gpumempct", "gpufbmem"};
@@ -23,7 +25,7 @@ class nvidiamon final : public Imonitor, public MessageBase {
                                         {"gpusmpct", "%", "%"},
                                         {"gpumempct", "%", "%"},
                                         {"gpufbmem", "kB", "kB"}};
-
+  
   // Map of classes that represent each monitored quantity
   // Will be initialised from the above parameter list
   prmon::monitored_list nvidia_stats;
@@ -32,36 +34,45 @@ class nvidiamon final : public Imonitor, public MessageBase {
   bool valid;
 
   // Count GPUs on the system
-  unsigned int ngpus;
+  static unsigned int ngpus;
+  
+  // Test if nvml is available
+  bool init_nvml();
+  
+  // Vectors to store utilization and memory info
+  std::vector<nvmlProcessUtilizationSample_t> utilization;
+  std::vector<nvmlProcessInfo_t> memory_info;
 
-  // Test if nvidia-smi is available
-  bool test_nvidia_smi();
+  // Max number of samples to take
+  const unsigned int max_samples = 100;
 
-  // Conversion from MB to kB for (this is to be more consistent with other
+  // Conversion from  B to kB for (this is to be more consistent with other
   // memory units in prmon)
-  const unsigned int MB_to_KB = 1024;
+  const unsigned int B_to_KB = 1024; 
+
+  // Last seen timestamp
+  unsigned long long last_seen_timestamp = 0;
 
  public:
   nvidiamon();
+  ~nvidiamon();
 
   void update_stats(const std::vector<pid_t>& pids,
-                    const std::string read_path = "");
-
-  // Alternative to cmd_pipe_output() for testing
-  std::pair<int, std::vector<std::string>> read_gpu_stats_test(
-      const std::string read_path);
-
+    const std::string read_path = "");
+    
   // These are the stat getter methods which retrieve current statistics
   prmon::monitored_value_map const get_text_stats();
   prmon::monitored_value_map const get_json_total_stats();
   prmon::monitored_average_map const get_json_average_stats(
-      unsigned long long elapsed_clock_ticks);
+        unsigned long long elapsed_clock_ticks);
   prmon::parameter_list const get_parameter_list();
 
   void const get_hardware_info(nlohmann::json& hw_json);
   void const get_unit_info(nlohmann::json& unit_json);
-  bool const is_valid() { return valid; }
+  bool const is_valid() { return valid; } 
+
 };
 REGISTER_MONITOR(Imonitor, nvidiamon, "Monitor NVIDIA GPU activity")
 
-#endif  // PRMON_NVIDIAMON_H
+
+#endif // PRMON_NVIDIAMON_H
