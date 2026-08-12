43fd5d3144e9ab70059d81bc99cc01d1f126953f
Graeme A Stewart <graeme.andrew.stewart@desy.de>
2026-08-12 20:03:16 +0200

    More robust parsing of GPU parameters (#318)
    * More robust parsing of GPU parameters
    
    GPU stats are obtained via an nvidia-smi call:
    
    nvidia-smi --query-gpu=clocks.max.sm,memory.total,gpu_name --format=csv,noheader,nounits
    
    That should give two numbers and a name.  However, as seen on some CERN
    GPU nodes, this is also possible:
    
    [N/A], 24576, NVIDIA H100L-2-24C
    
    Which is coming from a MIG partitioned GPU.
    
    The parser will now handle this case, as well as other parsing failures,
    by printing a warning and setting the frequency to 0.
    
    * Simplified and corrected implementation

--- package/src/nvidiamon.cpp
+++ package/src/nvidiamon.cpp
@@ -589,7 +589,18 @@ void const nvidiamon::get_hardware_info_smi(nlohmann::json& hw_json) {
   unsigned int count{0};
   for (auto const& s : cmd_result.second) {
     std::istringstream instr(s);
-    instr >> sm_freq;
+    // Parse first field to a string in case it's weird, like "[N/A]"
+    // N.B. We expect this to be digits + a comma
+    std::string tok;
+    instr >> tok;
+    errno = 0;
+    char* end = 0;
+    sm_freq = std::strtoul(tok.c_str(), &end, 10);
+    // Belt and braces error detection...
+    if ((end == tok.c_str()) || (*end != ',') || errno == ERANGE) {
+      warning("Odd value from GPU hardware query for frequency (will set to 0): " + tok);
+      sm_freq = 0;
+    }
     instr.get();  // Swallow the comma
     instr >> total_mem;
     auto pos = std::string::size_type(instr.tellg()) + 2;  // Skip ", "
